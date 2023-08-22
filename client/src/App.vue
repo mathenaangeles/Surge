b74332d54e88327a15f42effa59c7c4dbeff52a8
Joanne Veronica Arcilla <Joanne@Juns-MacBook-Pro.local>
2023-08-22 09:18:26 +0800

    Initial frontend setup - landing page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="test to Your Vue.js App"/>
+  <!-- NAVIGATION BAR -->
+  <nav class="navbar navbar-expand-lg navbar-dark u-navbar">
+    <div class="container-fluid">
+      <img src="@/assets/img/logo.png" height="40" style="padding:5px">
+    </div>
+  </nav>
+  <!-- AT A GLANCE BUTTON -->
+  <div style = "display: flex; justify-content:flex-end;">
+    <button class="vis-button"><p>At A Glance</p></button>
+  </div>
+  <!-- PROMPT AREA -->
+  <div class="prompt-area fixed-bottom">
+    <div class="mt-3 container-lg prompt-bar p-2">
+      <form>
+        <div class="d-flex flex-row">
+          <div class="custom-dropdown">
+            <select class="form-select text-light">
+              <option selected value="General">General</option>
+              <option value="Financials">Financials</option>
+              <option value="Technology">Technology</option>
+              <option value="Sustainability">Sustainability</option>
+              <option value="R&D">R&D</option>
+              <option value="MAStrategy">M&A, Strategy</option>
+            </select>
+          </div>
+          <div class="custom-promptbox">
+            <input type="text" class="form-control" id="prompt" placeholder="What do you want to know?">
+            <button type="submit" class="btn btn-warning btn-circle btn-sm">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+  <!-- TO RENDER HOMEPAGE UPON APP START -->
+  <Homepage/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Homepage from './components/Homepage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Homepage
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './assets/style.css';
 </style>
---
+++ client/src/components/Homepage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="main-wrapper container-fluid">
+  <div class="main row">
+    <div class="landing my-auto">
+      <div class="landing-text-group">
+      <img src="@/assets/img/chat.png" height="100">
+      <p style="font-family: 'ShillingM';font-size: 50px;">Hello, U!</p>
+      <p style="margin-top:-10px; line-height: 20px;">Need some help getting started? <br> Here are some questions you can ask:</p>
+      </div>
+      <div class="row suggested-group mx-auto">
+      <div class="col">
+        <div class="suggested">Suggested Question 1</div>
+        <div class="suggested mt-2">Suggestion Question 2</div>
+      </div>
+      <div class="col">
+        <div class="suggested">Suggested Question 3</div>
+        <div class="suggested mt-2">Suggestion Question 4</div>
+      </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Homepage',
+}
+</script>
