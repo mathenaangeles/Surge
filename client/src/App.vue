774c2008fb5a6ca041b86bfee38b67f782c84378
Joanne Veronica Arcilla <Joanne@Juns-MacBook-Pro.local>
2023-08-30 20:58:02 +0800

    front end updates

--- client/src/App.vue
+++ client/src/App.vue
@@ -37,7 +37,7 @@
             </select>
           </div>
           <div class="custom-promptbox">
-            <input v-model="question" type="text" class="form-control" id="prompt" placeholder="What do you want to know?">
+            <input v-model="question" @keydown.enter.prevent="fetchAnswer()" type="text" class="form-control" id="prompt" placeholder="What do you want to know?">
             <button type="submit" class="btn btn-warning btn-circle btn-sm">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
@@ -69,6 +69,7 @@ export default {
     const fetchAnswer = () => {
       console.log("QUESTION: " + question.value);
       console.log("AREA: " + area.value);
+      question.value = '';
     }
     const modalActive = ref(false);
     const toggleModal = () => {
