59e4d90a726577936c69fefffc5a43c0430beded
franciswguevara <69579186+franciswguevara@users.noreply.github.com>
2023-09-01 01:21:45 +0800

    Updated prompt engineering prefix
    - Step 1 works really well now thank you Abby and Mathena <3

--- surge/chatbot/utils.py
+++ surge/chatbot/utils.py
@@ -28,8 +28,11 @@ class ChatReadRetrieveReadApproach(ChatApproach):
 
     system_message_chat_conversation = """You are an intelligent assistant helping Unilever employees in the Competitor Intelligence group with their questions about the annual reports of Unilever and its competitors (for example: P&G, Colgate, etc.). Be brief in your answers.
 Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
-For tabular information return it as an html table. Do not return markdown format. If the question is not in English, answer in the language used in the question.
+When information can be tabulated, return it as an html table. Do not return markdown format. If the question is not in English, answer in the language used in the question.
+For ordered data, return it as a numbered list. For non-ordered data or summaries, use bullet points.
 Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brackets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
+Provide actionable insights and recommendations for the user. If a persona isn't specified, answer the question as if you were a data analyst for the competitor intelligence group at Unilever.
+When making recommendations and inferences explain your thought process step by step.
     
 {follow_up_questions_prompt}
 {injected_prompt}
@@ -39,21 +42,27 @@ Use double angle brackets to reference the questions, e.g. <<What strategies sho
 Try not to repeat questions that have already been asked.
 Only generate questions and do not generate any text before or after the questions, such as 'Next Questions'"""
 
-    query_prompt_template = """Below is a history of the conversation so far, and a new question asked by the user that needs to be answered by searching in a knowledge base about the annual reports of Unilever and its competitors.
-Generate a search query based on the conversation and the new question. The search query, when entered into Azure Cognitive Search using the Semantic Search method, should be able to generate a coherent answer based on the original user question.
+    query_prompt_template = """Below is a history of the conversation so far, and a new question asked by the user that needs to be answered by searching in a knowledge base about the annual reports of Unilever and its competitors. 
+Generate a new search query based on the conversation and the new question. The search query, when entered into Azure Cognitive Search using the Semantic Search method, should be able to generate a coherent answer based on the original user question.
 Do not include cited source filenames and document names e.g info.txt or doc.pdf in the search query terms.
 Do not include any text inside [] or <<>> in the search query terms.
 Do not include any special characters like '+'.
 If the question is not in English, translate the question to English before generating the search query.
 If you cannot generate a search query, return just the number 0.
+If the year is not specified, add "at least 5 years" to the search query. 
+If the company is not specified, specify that the search query is about "Unilever and its top 3 competitors". 
+Understand what the user is looking for and add related areas of analysis to the search query.
 """
     query_prompt_few_shots = [
         {'role' : USER, 'content' : "What is Unilever's sales?"},
-        {'role' : ASSISTANT, 'content' : "Show a detailed overview of Unilever's financial performance for the year 2021" },
+        {'role' : ASSISTANT, 'content' : "Unilever's Sales KPIs for the year 2021" },
+        {'role' : USER, 'content' : "What is P&G's sales for 2021?"},
+        {'role' : ASSISTANT, 'content' : "P&G's turnover in 2021" },
         {'role' : USER, 'content' : "What is the breakdown of turnover by Business Group?"},
-        {'role' : ASSISTANT, 'content' : "Show the components of the turnover of Unilever in 2021 by Business Group"},
+        {'role' : ASSISTANT, 'content' : "Components of the turnover of Unilever in 2021 by Business Group"},
         {'role': USER, 'content': "What is Unilever's sales for 2015-2019?"},
-        {'role': ASSISTANT, 'content':"Provide a detailed overview of Unilever's financial performance from 2015 to 2019, specifically focusing on their revenue or turnover during this period. Include insights into any growth or decline trends, major contributing factors, and a comparison with key competitors. Feel free to incorporate relevant data and statistics to support your analysis."}
+        {'role': ASSISTANT, 'content':"Unilever's financial performance from 2015 to 2019"}
+      
     ]
 
     def __init__(self, search_client: SearchClient, chatgpt_deployment: str, chatgpt_model: str, embedding_deployment: str, sourcepage_field: str, content_field: str):
@@ -89,12 +98,12 @@ If you cannot generate a search query, return just the number 0.
             deployment_id=self.chatgpt_deployment,
             model=self.chatgpt_model,
             messages=messages,
-            temperature=0.1,
-            max_tokens=5000,
+            temperature=0.5,
+            max_tokens=6000,
             n=1)
 
         query_text = chat_completion.choices[0].message.content
-        # query_text = user_q
+        query_text = query_text.replace('\"',"")
         if query_text.strip() == "0":
             query_text = history[-1]["user"] # Use the last user input if we failed to generate a better query
 
@@ -162,7 +171,7 @@ If you cannot generate a search query, return just the number 0.
             model=self.chatgpt_model,
             messages=messages,
             temperature=overrides.get("temperature") or 0.35,
-            max_tokens=1024,
+            max_tokens=8000,
             n=1)
 
         chat_content = chat_completion.choices[0].message.content
