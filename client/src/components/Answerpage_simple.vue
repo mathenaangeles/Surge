d40a85276ffbb4c5884ee5da9b8cf53228c42e34
Joanne Veronica Arcilla <joarcilla@up.edu.ph>
2023-09-11 23:47:32 +0800

    hardcoded ver finalizations

--- client/src/components/Answerpage_simple.vue
+++ client/src/components/Answerpage_simple.vue
@@ -9,7 +9,7 @@
         <br><br>
         <button @click="toggleRefined" class="btn btn-warning refined-btn" ref="btnToggle">Show refined question</button>
         <div class="refined-q mt-3" v-show="showRefined">
-          Factors contributing to Unilever's sales growth in 2017 Unilever and its top 3 competitors annual reports from 2017
+          Factors driving Unilever sales growth in 2017 OR Main drivers of Unilever's revenue increase in 2017 OR What caused Unilever's sales to grow in 2017? AND Unilever annual report AND competitor analysis
         </div>
       </div>
       <div class="area-containers mx-5 p-4 answer-area">
@@ -21,21 +21,26 @@
                 <path d="M2 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L7 13.101l-4.223 2.815A.5.5 0 0 1 2 15.5V4zm2-1a1 1 0 0 0-1 1v10.566l3.723-2.482a.5.5 0 0 1 .554 0L11 14.566V4a1 1 0 0 0-1-1H4z"/>
                 <path d="M4.268 1H12a1 1 0 0 1 1 1v11.768l.223.148A.5.5 0 0 0 14 13.5V2a2 2 0 0 0-2-2H6a2 2 0 0 0-1.732 1z"/>
               </svg>
-              [1] Unilever_2018_Annual_Report-11.pdf
+              [1] Unilever_2017_Annual_Report-19.pdf
             </span>
             <span class="source">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="source-icon bi bi-bookmarks" viewBox="0 0 16 16">
                 <path d="M2 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L7 13.101l-4.223 2.815A.5.5 0 0 1 2 15.5V4zm2-1a1 1 0 0 0-1 1v10.566l3.723-2.482a.5.5 0 0 1 .554 0L11 14.566V4a1 1 0 0 0-1-1H4z"/>
                 <path d="M4.268 1H12a1 1 0 0 1 1 1v11.768l.223.148A.5.5 0 0 0 14 13.5V2a2 2 0 0 0-2-2H6a2 2 0 0 0-1.732 1z"/>
               </svg>
-              [2] Unilever_2016_Annual_Report-51.pdf
+              [2] Unilever_2017_Annual_Report-11.pdf
             </span>
           </div>
         </div>
         <div class="mt-4">
-          Unilever has delivered an average underlying sales growth of 3.3% per year from 2014 to 2018 <sup class="inline-source">[1]</sup>. 
-          Additionally, Unilever has consistently driven for top- and bottom-line growth, with an average core operating margin improvement of 40 to 70 basis points per year <sup class="inline-source">[2]</sup><sup class="inline-source">[1]</sup>. <br><br>
-          Based on this information, it seems that Unilever's consistent focus on underlying sales growth and improving core operating margins has been the main driver of their sales growth in 2017. As a data analyst for the competitor intelligence group at Unilever, it may be worthwhile to investigate how competitors are driving their sales growth and whether there are any areas where Unilever can improve their strategy to stay competitive.
+          According to the Unilever 2017 Annual Report<sup class="inline-source">[1]</sup>, 
+          the main drivers of Unilever's sales growth in 2017 were underlying sales growth of 3.1% and underlying operating margin growth of 110 basis points to 17.5%. 
+          Additionally, Unilever increased dividends by 12%, which reflects the company's confidence in future profit growth and cash generation. 
+          The Unilever 2017 Annual Report <sup class="inline-source">[2]</sup> also notes that the company's strategy and business model continue to deliver consistent, competitive, profitable, and responsible growth. 
+          Between 2009 and 2017, Unilever delivered underlying sales growth of 4.3% per year, while operating margin expanded by 390 basis points to 16.5%.
+          <br><br>
+          Based on this information, it appears that Unilever's focus on consistent and responsible growth, as well as its ability to increase operating margins, were key drivers of its sales growth in 2017. 
+          Additionally, the company's confidence in future profit growth and cash generation, as reflected by its increased dividends, suggests that Unilever is well-positioned for continued growth in the future.
           <br>
           <hr/>
           <br>
@@ -51,6 +56,7 @@
             <router-link :to="{ path: '/' }">
             <button class="btn related-q">How do Unilever's sales compare to its competitors in the industry?</button>
             </router-link>
+            <br><br><br><br><br><br>
           </div>
           <br>
         </div>
